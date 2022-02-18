9dbbf21de4c3b43c19c52327442f7ef9ee677c9f
yumi <tea.with.mila@outlook.jp>
2022-02-18 07:28:46 +0000

    practice se

--- practice.php
+++ practice.php
@@ -44,3 +44,33 @@ $calendar_2018 = [
 
 // 12月を表示する
 echo $calendar_2018["December"] . "\n";
+
+$name = "藤巻由美";
+if ($name == "藤巻由美"){
+  echo "私は藤巻由美です" . "\n";
+} else {
+  echo "藤巻由美ではありません" . "\n";
+} 
+
+$total = 0;
+echo $total . "\n";
+
+for ($i = 1; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total. "\n";
+
+$fruits = array("peach","melon","orange","grape","banana");
+foreach($fruits as $a){
+  echo "要素は" . $a;
+  echo "\n";
+}
+
+$start = 1;
+$end = 100;
+for ($i = $start; $i <= $end; $i++) {
+  if ($i % 5 == 0) {
+    echo $i. "\n";
+  }
+}
+
